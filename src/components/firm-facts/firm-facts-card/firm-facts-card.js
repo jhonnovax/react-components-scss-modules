1c2437f4634af8d6738317485f31807e668b84b8
Jhon Nova <jhonnovax@gmail.com>
2024-08-20 22:50:11 -0700

    Added improvements for styles transitions and disabled state for cards

--- src/components/firm-facts/firm-facts-card/firm-facts-card.js
+++ src/components/firm-facts/firm-facts-card/firm-facts-card.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import styles from './firm-facts-card.module.scss';
 import FirmFactButton from '../firm-facts-button/firm-facts-button.js';
 
-const FirmFactsCard = ({ alignTop, disabled, className, variantCard, variantButton, buttonText }) => {
+const FirmFactsCard = ({ disabled, className, variantCard, variantButton, buttonText }) => {
 	const [isBtnHovered, setIsBtnHovered] = useState(false);
 	const cardClass = variantCard === 'border' ? styles.border : styles.default;
+	const disabledClass = disabled ? styles.disabled : '';
 
 	return (
 		<div 
-			className={`${className} ${styles.card} ${cardClass}`}
+			className={`${className} ${styles.card} ${cardClass} ${disabledClass}`}
 			onMouseEnter={() => setIsBtnHovered(true)}
 			onMouseLeave={() => setIsBtnHovered(false)}
 		>
